15edd692b9a00d6afc78620e15cd713a3a7685f7
Edward259 <yjw@onyx-international.com>
2021-06-16 20:16:36 +0800

    feat(OnyxAndroidDemo):add result display for eac page

--- app/src/main/java/com/onyx/android/demo/EacDemoActivity.java
+++ app/src/main/java/com/onyx/android/demo/EacDemoActivity.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.onyx.android.sdk.api.device.eac.SimpleEACManage;
 import com.onyx.android.sdk.device.Device;
 import com.onyx.android.sdk.rx.RxUtils;
 
+import java.util.concurrent.Callable;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class EacDemoActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.hook_epdc)
     Button hookEpdc;
+    @Bind(R.id.hook_epdc_status)
+    TextView hookEpdcStatus;
     @Bind(R.id.eac_enable)
     Button eacEnable;
+    @Bind(R.id.eac_enable_status)
+    TextView eacEnableStatus;
     @Bind(R.id.allow_eac)
     Button allowEac;
     @Bind(R.id.disallow_eac)
@@ -36,24 +45,15 @@ public class EacDemoActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         if (v.equals(hookEpdc)) {
-            RxUtils.runInIO(new Runnable() {
-                @Override
-                public void run() {
-                    boolean hookEpdc = SimpleEACManage.getInstance().isHookEpdc(getPackageName());
-                }
-            });
+            updateHookEpdaStatus();
         } else if (v.equals(eacEnable)) {
-            RxUtils.runInIO(new Runnable() {
-                @Override
-                public void run() {
-                    boolean appEACEnabled = SimpleEACManage.getInstance().isAppEACEnabled(getPackageName());
-                }
-            });
+            updateEacSwitchStatus();
         } else if (v.equals(allowEac)) {
             RxUtils.runInIO(new Runnable() {
                 @Override
                 public void run() {
                     Device.currentDevice().unregisterEACWhiteList(getApplicationContext());
+                    updateHookEpdaStatus();
                 }
             });
         } else if (v.equals(disallowEac)) {
@@ -61,8 +61,42 @@ public class EacDemoActivity extends AppCompatActivity implements View.OnClickLi
                 @Override
                 public void run() {
                     Device.currentDevice().registerEACWhiteList(getApplicationContext());
+                    updateHookEpdaStatus();
                 }
             });
         }
+    }
+
+    /**
+     *It`s app optimize switch status, not about with eac enable/disable.
+     */
+    private void updateEacSwitchStatus() {
+        RxUtils.runWith(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return SimpleEACManage.getInstance().isAppEACEnabled(getPackageName());
+
+            }
+        }, new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                eacEnableStatus.setText(aBoolean + "");
+            }
+        }, Schedulers.io());
+    }
+
+    private void updateHookEpdaStatus() {
+        RxUtils.runWith(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return SimpleEACManage.getInstance().isHookEpdc(getPackageName());
+
+            }
+        }, new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                hookEpdcStatus.setText(aBoolean + "");
+            }
+        }, Schedulers.io());
     }
 }
