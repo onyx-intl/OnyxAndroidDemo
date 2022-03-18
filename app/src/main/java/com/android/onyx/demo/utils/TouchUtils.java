4150a0d92bf65fbf2ceb960b5d90954c1c3b830c
chenhaijiang <chj@onyx-international.com>
2022-03-18 16:42:50 +0800

    fix:remove isCTPPowerOn check

--- app/src/main/java/com/android/onyx/demo/utils/TouchUtils.java
+++ app/src/main/java/com/android/onyx/demo/utils/TouchUtils.java
@@ -15,20 +15,14 @@ import com.onyx.android.sdk.api.device.epd.EpdController;
 public class TouchUtils {
 
     public static void disableFingerTouch(Context context) {
-        boolean canFingerTouch = EpdController.isCTPPowerOn();
-        if (canFingerTouch) {
-            int width = context.getResources().getDisplayMetrics().widthPixels;
-            int height = context.getResources().getDisplayMetrics().heightPixels;
-            Rect rect = new Rect(0, 0, width, height);
-            Rect[] arrayRect =new Rect[]{rect};
-            EpdController.setAppCTPDisableRegion(context, arrayRect);
-        }
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+        int height = context.getResources().getDisplayMetrics().heightPixels;
+        Rect rect = new Rect(0, 0, width, height);
+        Rect[] arrayRect =new Rect[]{rect};
+        EpdController.setAppCTPDisableRegion(context, arrayRect);
     }
 
     public static void enableFingerTouch(Context context) {
-        boolean canFingerTouch = !EpdController.isCTPPowerOn();
-        if (!canFingerTouch) {
-            EpdController.appResetCTPDisableRegion(context);
-        }
+        EpdController.appResetCTPDisableRegion(context);
     }
 }
