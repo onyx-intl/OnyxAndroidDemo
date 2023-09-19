f52728c9c1ca1b760fe7dd4ec4d18dfc6105df49
Edward259 <yjw@onyx-international.com>
2023-09-19 15:13:38 +0800

    feat:update onyxsdk-device ot 1.2.26 and update light demo.

--- app/src/main/java/com/android/onyx/demo/FrontLightDemoActivity.java
+++ app/src/main/java/com/android/onyx/demo/FrontLightDemoActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.databinding.DataBindingUtil;
 
 import com.onyx.android.demo.R;
 import com.onyx.android.demo.databinding.ActivityFrontLightDemoBinding;
-import com.onyx.android.sdk.api.device.FrontLightController;
+import com.onyx.android.sdk.api.device.brightness.BaseBrightnessProvider;
+import com.onyx.android.sdk.api.device.brightness.BrightnessController;
+import com.onyx.android.sdk.api.device.brightness.BrightnessType;
 import com.onyx.android.sdk.device.BaseDevice;
+import com.onyx.android.sdk.utils.RxTimerUtil;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class FrontLightDemoActivity extends AppCompatActivity {
     private ActivityFrontLightDemoBinding binding;
+    private BrightnessType brightnessType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,88 +32,179 @@ public class FrontLightDemoActivity extends AppCompatActivity {
         binding.setActivityFrontlight(this);
     }
 
-    private void initView() {
-        if (FrontLightController.hasCTMBrightness(this)) {
-            binding.warmLightValue.setText("Current value:" + FrontLightController.getWarmLightConfigValue(this));
-            binding.coldLightValue.setText("Current value:" + FrontLightController.getColdLightConfigValue(this));
-            binding.flCover.setVisibility(View.VISIBLE);
-
-        } else if (FrontLightController.hasFLBrightness(this)) {
-            binding.lightValue.setText("Current value:" + FrontLightController.getBrightness(this));
-            binding.ctmCover.setVisibility(View.VISIBLE);
-
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            updateState();
         }
+    }
+
+    private void updateState() {
+        switch (getBrightnessType()) {
+            case FL:
+                updateFLValue();
+                break;
+            case WARM_AND_COLD:
+                updateWarmAndColdValue();
+                break;
+            case CTM:
+                updateCTMValue();
+                break;
+            case NONE:
+            default:
+                break;
+        }
+    }
+
+    private void initView() {
+        switch (getBrightnessType()) {
+            case FL:
+                initFLTypeView();
+                break;
+            case WARM_AND_COLD:
+                initWarmAndColdTypeView();
+                break;
+            case CTM:
+                initCTMAllTypeView();
+                break;
+            case NONE:
+            default:
+                break;
+        }
+    }
+
+    public BrightnessType getBrightnessType() {
+        if (brightnessType == null) {
+            brightnessType = BrightnessController.getBrightnessType(this);
+        }
+        return brightnessType;
+    }
+
+    //======================================= CTM ALL =======================================
+
+    private BaseBrightnessProvider temperatureProvider;
+    private BaseBrightnessProvider ctmBrightnessProvider;
+
+    private void initCTMAllTypeView() {
+        binding.ctmAllContainer.setVisibility(View.VISIBLE);
+        temperatureProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_TEMPERATURE);
+        initSeekBar(binding.ctmAllTemperatureSeek, binding.temperatureValue, temperatureProvider);
+
+        ctmBrightnessProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_BRIGHTNESS);
+        initSeekBar(binding.ctmAllBrightnessSeek, binding.brightnessValue, ctmBrightnessProvider);
+    }
+
+    private void updateCTMValue() {
+        binding.ctmAllBrightnessSeek.setProgress(ctmBrightnessProvider.getIndex());
+        binding.ctmAllTemperatureSeek.setProgress(temperatureProvider.getIndex());
+    }
+
+    public void toggleCTMLight(View view) {
+        ctmBrightnessProvider.toggle();
+        delay(new RxTimerUtil.TimerObserver() {
+            @Override
+            public void onNext(Long aLong) {
+                updateCTMValue();
+            }
+        });
+    }
+
+    //=======================================  Warm And Cold =======================================
+
+    private BaseBrightnessProvider warmProvider;
+    private BaseBrightnessProvider coldProvider;
+
+    private void initWarmAndColdTypeView() {
+        binding.warmColdContainer.setVisibility(View.VISIBLE);
+        warmProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_WARM);
+        initSeekBar(binding.warmBrightnessSeek, binding.warmBrightnessValue, warmProvider);
+
+        coldProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_COLD);
+        initSeekBar(binding.coldBrightnessSeek, binding.coldBrightnessValue, coldProvider);
+    }
+
+    private void updateWarmAndColdValue() {
+        binding.warmBrightnessSeek.setProgress(warmProvider.getIndex());
+        binding.coldBrightnessSeek.setProgress(coldProvider.getIndex());
     }
 
     public void toggleWarmLight(View view) {
-        if (FrontLightController.isWarmLightOn(this)) {
-            FrontLightController.closeWarmLight();
-        } else {
-            FrontLightController.openWarmLight();
-        }
-    }
-
-    public void increaseWarmLight(View view) {
-        increaseBrightness(FrontLightController.LIGHT_TYPE_CTM_WARM);
-    }
-
-    public void decreaseWarmLight(View view) {
-        decreaseBrightness(FrontLightController.LIGHT_TYPE_CTM_WARM);
+        warmProvider.toggle();
+        delay(new RxTimerUtil.TimerObserver() {
+            @Override
+            public void onNext(Long aLong) {
+                updateWarmAndColdValue();
+            }
+        });
     }
 
     public void toggleColdLight(View view) {
-        if (FrontLightController.isColdLightOn(this)) {
-            FrontLightController.closeColdLight();
-        } else {
-            FrontLightController.openColdLight();
-        }
+        coldProvider.toggle();
+        delay(new RxTimerUtil.TimerObserver() {
+            @Override
+            public void onNext(Long aLong) {
+                updateWarmAndColdValue();
+            }
+        });
     }
 
-    public void updateWarmLightValue(View view) {
-        binding.warmLightValue.setText("Current value:" + FrontLightController.getWarmLightConfigValue(this));
+    //=======================================  FL  =======================================
+
+    private BaseBrightnessProvider flProvider;
+
+    private void initFLTypeView() {
+        binding.flContainer.setVisibility(View.VISIBLE);
+        flProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_FL);
+        initSeekBar(binding.flBrightnessSeek, binding.flBrightnessValue, flProvider);
     }
 
-    public void increaseColdLight(View view) {
-        increaseBrightness(FrontLightController.LIGHT_TYPE_CTM_COLD);
-    }
-
-    public void decreaseColdLight(View view) {
-        decreaseBrightness(FrontLightController.LIGHT_TYPE_CTM_COLD);
+    public void updateFLValue() {
+        binding.flBrightnessSeek.setProgress(flProvider.getIndex());
     }
 
     public void toggleFLLight(View view) {
-        if (FrontLightController.isLightOn(this, BaseDevice.LIGHT_TYPE_FL)) {
-            FrontLightController.turnOff(this);
-        } else {
-            FrontLightController.turnOn(this);
-        }
+        flProvider.toggle();
+        delay(new RxTimerUtil.TimerObserver() {
+            @Override
+            public void onNext(Long aLong) {
+                updateFLValue();
+            }
+        });
     }
 
-    public void updateColdLightValue(View view) {
-        binding.coldLightValue.setText("Current value:" + FrontLightController.getColdLightConfigValue(this));
-    }
+    public void initSeekBar(SeekBar seekBar, TextView tvLightValue, BaseBrightnessProvider provider) {
+        seekBar.setMax(provider.getMaxIndex());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    provider.setIndex(progress);
+                }
+                updateValue(tvLightValue, provider.getValueByIndex(progress));
+            }
 
-    public void decreaseFLLight(View view) {
-        decreaseBrightness(FrontLightController.LIGHT_TYPE_FL);
-    }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-    public void increaseFLLight(View view) {
-        increaseBrightness(FrontLightController.LIGHT_TYPE_FL);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar.setProgress(provider.getIndex());
     }
 
     public void ShowBrightnessSetting(View view) {
         sendBroadcast(new Intent("action.show.brightness.dialog"));
     }
 
-    public void updateLightValue(View view) {
-        binding.lightValue.setText("Current value:" + FrontLightController.getBrightness(this));
+    private void updateValue(TextView textView, int value) {
+        textView.setText("Current value:" + value);
     }
 
-    public void increaseBrightness(int colorTemp) {
-        FrontLightController.increaseBrightness(this, colorTemp);
-    }
-
-    public void decreaseBrightness(int colorTemp) {
-        FrontLightController.decreaseBrightness(this, colorTemp);
+    private void delay(RxTimerUtil.TimerObserver timerObserver) {
+        RxTimerUtil.timer(100, TimeUnit.MILLISECONDS, timerObserver);
     }
 }
