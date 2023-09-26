859f7aa5e420fb5f153e42f17dba0f7b1b280613
Edward259 <yjw@onyx-international.com>
2023-09-26 14:42:21 +0800

    feat:add monitoring of changes in front light values.

--- app/src/main/java/com/android/onyx/demo/FrontLightDemoActivity.java
+++ app/src/main/java/com/android/onyx/demo/FrontLightDemoActivity.java
@@ -1,8 +1,11 @@
 package com.android.onyx.demo;
 
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -21,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class FrontLightDemoActivity extends AppCompatActivity {
+    private static final String TAG = FrontLightDemoActivity.class.getSimpleName();
     private ActivityFrontLightDemoBinding binding;
     private BrightnessType brightnessType;
 
@@ -81,6 +85,10 @@ public class FrontLightDemoActivity extends AppCompatActivity {
     }
 
     //======================================= CTM ALL =======================================
+    private static final String KEY_CTM_BRIGHTNESS = "screen_ctm_brightness";
+    private static final String KEY_CTM_TEMPERATURE = "screen_ctm_temperature";
+    private static final String KEY_CTM_BRIGHTNESS_STATE = "ctm_brightness_state_key";
+    private static final String KEY_CTM_TEMPERATURE_STATE = "ctm_temperature_state_key";
 
     private BaseBrightnessProvider temperatureProvider;
     private BaseBrightnessProvider ctmBrightnessProvider;
@@ -92,6 +100,30 @@ public class FrontLightDemoActivity extends AppCompatActivity {
 
         ctmBrightnessProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_BRIGHTNESS);
         initSeekBar(binding.ctmAllBrightnessSeek, binding.brightnessValue, ctmBrightnessProvider);
+        registerObserver(KEY_CTM_BRIGHTNESS, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                binding.ctmAllBrightnessSeek.setProgress(ctmBrightnessProvider.getIndex());
+            }
+        });
+        registerObserver(KEY_CTM_TEMPERATURE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                binding.ctmAllTemperatureSeek.setProgress(temperatureProvider.getIndex());
+            }
+        });
+        registerObserver(KEY_CTM_BRIGHTNESS_STATE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                Log.i(TAG, "CTM brightness light on: " + ctmBrightnessProvider.isLightOn());
+            }
+        });
+        registerObserver(KEY_CTM_TEMPERATURE_STATE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                Log.i(TAG, "CTM temperature light on: " + temperatureProvider.isLightOn());
+            }
+        });
     }
 
     private void updateCTMValue() {
@@ -110,6 +142,10 @@ public class FrontLightDemoActivity extends AppCompatActivity {
     }
 
     //=======================================  Warm And Cold =======================================
+    private static final String KEY_WARM_BRIGHTNESS = "screen_warm_brightness";
+    private static final String KEY_COLD_BRIGHTNESS = "screen_cold_brightness";
+    private static final String KEY_WARM_BRIGHTNESS_STATE = "warm_brightness_state_key";
+    private static final String KEY_COLD_BRIGHTNESS_STATE = "cold_brightness_state_key";
 
     private BaseBrightnessProvider warmProvider;
     private BaseBrightnessProvider coldProvider;
@@ -121,6 +157,31 @@ public class FrontLightDemoActivity extends AppCompatActivity {
 
         coldProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_CTM_COLD);
         initSeekBar(binding.coldBrightnessSeek, binding.coldBrightnessValue, coldProvider);
+
+        registerObserver(KEY_COLD_BRIGHTNESS, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                binding.coldBrightnessSeek.setProgress(coldProvider.getIndex());
+            }
+        });
+        registerObserver(KEY_WARM_BRIGHTNESS, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                binding.warmBrightnessSeek.setProgress(warmProvider.getIndex());
+            }
+        });
+        registerObserver(KEY_COLD_BRIGHTNESS_STATE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                Log.i(TAG, "Cold brightness light on: " + coldProvider.isLightOn());
+            }
+        });
+        registerObserver(KEY_WARM_BRIGHTNESS_STATE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                Log.i(TAG, "Warm brightness light on: " + warmProvider.isLightOn());
+            }
+        });
     }
 
     private void updateWarmAndColdValue() {
@@ -150,12 +211,28 @@ public class FrontLightDemoActivity extends AppCompatActivity {
 
     //=======================================  FL  =======================================
 
+    private static final String KEY_FL_BRIGHTNESS_STATE = "screen_brightness";
+    private static final String KEY_FL_BRIGHTNESS = "screen_cold_brightness";
+
     private BaseBrightnessProvider flProvider;
 
     private void initFLTypeView() {
         binding.flContainer.setVisibility(View.VISIBLE);
         flProvider = BrightnessController.getBrightnessProvider(this, BaseDevice.LIGHT_TYPE_FL);
         initSeekBar(binding.flBrightnessSeek, binding.flBrightnessValue, flProvider);
+
+        registerObserver(KEY_FL_BRIGHTNESS, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                binding.flBrightnessSeek.setProgress(flProvider.getIndex());
+            }
+        });
+        registerObserver(KEY_FL_BRIGHTNESS_STATE, new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                Log.i(TAG, "Cold brightness light on: " + flProvider.isLightOn());
+            }
+        });
     }
 
     public void updateFLValue() {
@@ -206,5 +283,9 @@ public class FrontLightDemoActivity extends AppCompatActivity {
 
     private void delay(RxTimerUtil.TimerObserver timerObserver) {
         RxTimerUtil.timer(100, TimeUnit.MILLISECONDS, timerObserver);
+    }
+
+    private void registerObserver(String key, ContentObserver contentObserver) {
+        getContentResolver().registerContentObserver(Settings.System.getUriFor(key), false, contentObserver);
     }
 }
